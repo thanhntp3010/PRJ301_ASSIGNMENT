e0ac53ef88eda16bc09a6d6d2475a92b2fa33836
thanhntp3010 <cnguyenphuongthanh@gmail.com>
2024-07-08 22:49:58 +0700

    update GradeDAO for get Student grade

--- Assignment/src/java/dao/GradeDAO.java
+++ Assignment/src/java/dao/GradeDAO.java
@@ -5,6 +5,7 @@
 package dao;
 
 import dto.ReportDTO;
+import dto.StudentCourseInfo;
 import entity.Grade;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -177,77 +178,6 @@ public class GradeDAO {
         }
 
         return reportDTOs;
-    }
-
-    public class StudentCourseInfo {
-
-        private int no;
-        private String semester;
-        private String subjectCode;
-        private String subjectName;
-        private double grade;
-        private String status;
-
-        public StudentCourseInfo() {
-        }
-
-        public StudentCourseInfo(int no, String semester, String subjectCode, String subjectName, double grade, String status) {
-            this.no = no;
-            this.semester = semester;
-            this.subjectCode = subjectCode;
-            this.subjectName = subjectName;
-            this.grade = grade;
-            this.status = status;
-        }
-
-        public int getNo() {
-            return no;
-        }
-
-        public void setNo(int no) {
-            this.no = no;
-        }
-
-        public String getSemester() {
-            return semester;
-        }
-
-        public void setSemester(String semester) {
-            this.semester = semester;
-        }
-
-        public String getSubjectCode() {
-            return subjectCode;
-        }
-
-        public void setSubjectCode(String subjectCode) {
-            this.subjectCode = subjectCode;
-        }
-
-        public String getSubjectName() {
-            return subjectName;
-        }
-
-        public void setSubjectName(String subjectName) {
-            this.subjectName = subjectName;
-        }
-
-        public double getGrade() {
-            return grade;
-        }
-
-        public void setGrade(double grade) {
-            this.grade = grade;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
     }
 
 }
